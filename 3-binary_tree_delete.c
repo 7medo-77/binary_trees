5d2e2a8ff7f4e34c8939215f1d2f5b25c29bdbca
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-27 00:36:06 +0200

    3-binary_tree_delete.c_0

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - Deletes a whole binary tree
+ *
+ * @tree: pointer to the root node of the tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	free(tree);
+	binary_tree_delete(tree->right);
+	free(tree);
+	free(tree);
+}
