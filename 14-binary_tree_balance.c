34c202d03521599a2c948366de0fbd890f2acaf2
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-27 23:10:43 +0200

    14-binary_tree_leaves.c_1

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,44 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Checks height of tree from the node
+ *
+ * @tree: Pointer to the root of the binary tree
+ *
+ * Return: height of the tree form the node
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right, height;
+
+	if (!tree)
+		return (-1);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	height = left + 1 >= right + 1 ? left + 1 : right + 1;
+
+	return (height);
+}
+
+
+/**
+ * binary_tree_balance - Checks number of nodes with at least one child
+ *
+ * @tree: Pointer to the root of the binary tree
+ *
+ * Return: Number of nodes in the tree with at least one child
+ */
+size_t binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left, right, size;
+
+	if (!tree)
+		return (-1);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	size = left - right;
+
+	return (size);
+}
